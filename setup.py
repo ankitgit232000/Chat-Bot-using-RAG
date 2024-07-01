1cd2af11b7d086f22e967eeeb768df3e6caf35e4
Ankit Kumar Singh <59502602+ankysingh23@users.noreply.github.com>
2024-07-01 11:36:53 +0530

    Setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,17 @@
 from setuptools import find_packages , setup
 from typing import List
+HYPEN_E_DOT="-e ."
 def get_requirements(file_path:str)->List[str]:
     """this function will return the list of requirement"""
-    requirement=[]
+    requirements=[]
     with open(file_path) as file_obj:
         requirements=file_obj.readlines()
+        requirements=[req.replace('\n','') for req in requirements]
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
 
 setup(
-    name=Chat-Bot-using-RAG,
+    name='Chat Bot using RAG',
     version="0.0.1",
     author="Ankit",
     author_email="ankitdude2000@gmail.com",
